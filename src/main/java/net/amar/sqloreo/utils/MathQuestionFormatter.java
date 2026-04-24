1c90d97a7a51d6b851e3cde5f5beb9f819e0d0d1
AmarKherala <markhyrallh6@gmail.com>
2026-04-25 01:45:31 +0300

    formatted files and added getters

--- src/main/java/net/amar/sqloreo/utils/MathQuestionFormatter.java
+++ src/main/java/net/amar/sqloreo/utils/MathQuestionFormatter.java
@@ -4,43 +4,45 @@ import java.util.List;
 
 public class MathQuestionFormatter {
 
-    private static final String[] NUMS = {
-            "zero","one","two","three","four","five","six","seven","eight","nine",
-            "ten","eleven","twelve","thirteen","fourteen","fifteen",
-            "sixteen","seventeen","eighteen","nineteen"
+  private static final String[] NUMS = {
+      "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine",
+      "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen",
+      "sixteen", "seventeen", "eighteen", "nineteen"
+  };
+
+  private static final String[] TENS = {
+      "", "", "twenty", "thirty", "forty", "fifty"
+  };
+
+  public static String number(int n) {
+    if (n < 20)
+      return NUMS[n];
+    if (n % 10 == 0)
+      return TENS[n / 10];
+    return TENS[n / 10] + "-" + NUMS[n % 10];
+  }
+
+  public static String operator(char op) {
+    return switch (op) {
+      case '+' -> "plus";
+      case '-' -> "minus";
+      case '*' -> "times";
+      default -> "";
     };
+  }
 
-    private static final String[] TENS = {
-            "", "", "twenty", "thirty", "forty", "fifty"
-    };
+  public static String expressionBuilder(List<Integer> numbers, List<Character> ops) {
+    StringBuilder sb = new StringBuilder();
 
-    public static String number(int n) {
-        if (n < 20) return NUMS[n];
-        if (n % 10 == 0) return TENS[n / 10];
-        return TENS[n / 10] + "-" + NUMS[n % 10];
+    sb.append(number(numbers.get(0)));
+
+    for (int i = 0; i < ops.size(); i++) {
+      sb.append(" ")
+          .append(operator(ops.get(i)))
+          .append(" ")
+          .append(number(numbers.get(i + 1)));
     }
 
-    public static String operator(char op) {
-        return switch (op) {
-            case '+' -> "plus";
-            case '-' -> "minus";
-            case '*' -> "times";
-            default -> "";
-        };
-    }
-
-    public static String expressionBuilder(List<Integer> numbers, List<Character> ops) {
-        StringBuilder sb = new StringBuilder();
-
-        sb.append(number(numbers.get(0)));
-
-        for (int i = 0; i < ops.size(); i++) {
-            sb.append(" ")
-                    .append(operator(ops.get(i)))
-                    .append(" ")
-                    .append(number(numbers.get(i + 1)));
-        }
-
-        return sb.toString();
-    }
+    return sb.toString();
+  }
 }
--- src/main/java/net/amar/sqloreo/utils/MathQuestionUtils.java
+++ src/main/java/net/amar/sqloreo/utils/MathQuestionUtils.java
@@ -6,77 +6,85 @@ import java.util.Random;
 
 public class MathQuestionUtils {
 
-  
-   public static class Question {
-        List<Integer> numbers;
-        List<Character> ops;
+  public static class Question {
+    List<Integer> numbers;
+    List<Character> ops;
 
-        public Question(List<Integer> numbers, List<Character> ops) {
-            this.numbers = numbers;
-            this.ops = ops;
-        }
+    public Question(List<Integer> numbers, List<Character> ops) {
+      this.numbers = numbers;
+      this.ops = ops;
     }
 
-    public static Question generateQ(int level) {
-        Random ran = new Random();
-        int arraySize;
-
-        switch (level) {
-            default:
-            case 1:
-                arraySize = 2;
-                break;
-            case 2:
-                arraySize = ran.nextInt(2) + 3;
-                break;
-            case 3:
-                arraySize = ran.nextInt(3) + 5;
-                break;
-        }
-
-        List<Integer> numbers = new ArrayList<>();
-        List<Character> ops = new ArrayList<>();
-
-        char[] opsPool = {'+', '-', '*'};
-
-        for (int i = 0; i < arraySize; i++) {
-            numbers.add(ran.nextInt(50));
-        }
-
-        for (int i = 0; i < arraySize - 1; i++) {
-            ops.add(opsPool[ran.nextInt(opsPool.length)]);
-        }
-
-        return new Question(numbers, ops);
+    public List<Integer> getNumbers() {
+      return numbers;
     }
 
-    public static int solveQ(Question q) {
-
-        List<Integer> nums = new ArrayList<>(q.numbers);
-        List<Character> ops = new ArrayList<>(q.ops);
-
-        for (int i = 0; i < ops.size(); i++) {
-            if (ops.get(i) == '*') {
-                int multiplied = nums.get(i) * nums.get(i + 1);
-                nums.set(i, multiplied);
-
-                nums.remove(i + 1);
-                ops.remove(i);
-
-                i--;
-            }
-        }
-
-
-        int result = nums.get(0);
-        for (int i = 0; i < ops.size(); i++) {
-            char op = ops.get(i);
-            int num = nums.get(i + 1);
-
-            if (op == '+') result += num;
-            else if (op == '-') result -= num;
-        }
-
-        return result;
+    public List<Character> getOps() {
+      return ops;
     }
+  }
+
+  public static Question generateQ(int level) {
+    Random ran = new Random();
+    int arraySize;
+
+    switch (level) {
+      default:
+      case 1:
+        arraySize = 2;
+        break;
+      case 2:
+        arraySize = ran.nextInt(2) + 3;
+        break;
+      case 3:
+        arraySize = ran.nextInt(3) + 5;
+        break;
+    }
+
+    List<Integer> numbers = new ArrayList<>();
+    List<Character> ops = new ArrayList<>();
+
+    char[] opsPool = { '+', '-', '*' };
+
+    for (int i = 0; i < arraySize; i++) {
+      numbers.add(ran.nextInt(50));
+    }
+
+    for (int i = 0; i < arraySize - 1; i++) {
+      ops.add(opsPool[ran.nextInt(opsPool.length)]);
+    }
+
+    return new Question(numbers, ops);
+  }
+
+  public static int solveQ(Question q) {
+
+    List<Integer> nums = new ArrayList<>(q.numbers);
+    List<Character> ops = new ArrayList<>(q.ops);
+
+    for (int i = 0; i < ops.size(); i++) {
+      if (ops.get(i) == '*') {
+        int multiplied = nums.get(i) * nums.get(i + 1);
+        nums.set(i, multiplied);
+
+        nums.remove(i + 1);
+        ops.remove(i);
+
+        i--;
+      }
+    }
+
+    int result = nums.get(0);
+    for (int i = 0; i < ops.size(); i++) {
+      char op = ops.get(i);
+      int num = nums.get(i + 1);
+
+      if (op == '+')
+        result += num;
+      else if (op == '-')
+        result -= num;
+    }
+
+    return result;
+  }
 }
